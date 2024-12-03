9bcee39bae89f183c2c3738b9597136542125060
usama-bit137 <usamaaqeel98@gmail.com>
2024-12-03 21:58:30 +0000

    Added functionality to change the symmetry-breaking term to potential.

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ sns.set_theme(style = "white")
 def energy_density(x, v, t, w, r):
     return (0.5*v**2 + potential_corr(x, w, r))
 
-def potential(x,w,r): 
+def potential(x, w, r): 
     return (1/4)*(x**2-(1-r))**2 + w*(x-1)
 
 def potential_derivative(x,w,r):
@@ -34,9 +34,9 @@ def Newton_Raphson(x, w, r, n):
     return x
 
 def ODE(x, v, t, w, r):
-    t_s = 10
-    if t<t_s: 
-        r = 0.9
+    t_s = 0.1
+    if t < t_s: 
+        r = 10
     else: 
         r = 0
     return -2*v/t + potential_derivative(x, w, r)
@@ -79,22 +79,35 @@ def IntBisec(a_u, a_o, E, r, N):
     return amid
 
 
-def analytical_approx(t, t_s, r_s): 
+def analytical_approx(t, t_s, r_s, w): 
     array = np.zeros(len(t))
+    
+    m_0 = 1
+    phi_0 = Newton_Raphson(1, w, 0, 100)
 
+    ax1.axvline(1/m_0, 
+                label="$\lambda_0 = (\sqrt{2}\mu)^{-1}$", 
+                linestyle= "dotted", 
+                color="darkgreen")
+    
+    ax1.axvline(t_s, 
+                label="$r_s=$" + str(0.1)+ "$\lambda_0$", 
+                linestyle= "dotted", 
+                color = "mediumvioletred")
+    
     for i in range(len(t)):
-        if t[i] < t_s: 
-            C = (1/t_s)*(1/np.cosh(np.sqrt(r_s)*t_s))
+        if t[i] < t_s:
+            C = (phi_0/(t_s*np.sqrt(r_s)))*(1/np.cosh(np.sqrt(r_s)*t_s))
             array[i] = C*(t_s/t[i])*np.sinh(np.sqrt(r_s)*t[i])
-        else: 
-            K = -(1-(1/(t_s*np.sqrt(r_s))*np.tanh(np.sqrt(r_s)*t_s)))
-            array[i] = K*(t_s/t[i])*np.exp(-np.sqrt(2)*(t[i]-t_s))+1
+        else:
+            K = -phi_0*(1-(1/(t_s*np.sqrt(r_s))*np.tanh(np.sqrt(r_s)*t_s)))
+            array[i] = K*(t_s/t[i])*np.exp(-m_0*(t[i]-t_s)) + phi_0
 
     return array
 
 # Fundamental values we require:
 N = 1
-E = np.linspace(0, 0.09, N)
+E = np.linspace(0.0, 0.09, N)
 r = 0
 
 # Initial conditions for the Runge-Kutta algorithm.
@@ -106,14 +119,13 @@ t_range = 50
 S_Euclidean = []
 
 for i in range(N):    
-    
-    x_range = 2
-    
-    a_u = np.float128(-0.1)
+    x_range = 2    
+    a_u = np.float128(+0.1)
     a_o = np.float128(Newton_Raphson(-0.9, E[i], r, 1))
+    phi_0 = np.float128(Newton_Raphson(1, E[i], r, 20))
     
     a_mid = IntBisec(a_u, a_o, E[i], r, 100)
-        
+    
     phi_mid = RK_22(a_mid, t0, v0, t_range, x_range, E[i], r)
     x_pot = np.linspace(-1.5,1.5,100)
 
@@ -121,40 +133,43 @@ for i in range(N):
     x = phi_mid[1]
     v = phi_mid[-1]
     
-    #search algorithm
+    #search algorithm: 
     n = 0
     for j in range(len(t)): 
         if abs(t[j] - 30) < 0.000001: 
             n=j
             break
-        
-    
-    t_cut = t[:30000]
-    x_cut = x[:30000]
-    v_cut = v[:30000]
-    
-    # Finding the index of intersection:
-    #for k in np.arange(0, len(x) - 1):
-     #   if np.sign(phi_mid[1, k]) != np.sign(phi_mid[1, k + 1]):
-      #      m = round(k)
-       #     break
 
-    #R = 0.5 * (phi_mid[0, m] + phi_mid[0, m + 1])
+    t_cut = t[:25000]
+    x_cut = x[:25000]
+    v_cut = v[:25000]
     
-        
     dE_dr = energy_density(x_cut, v_cut, t_cut, E[i], r)
     
-    ax1.plot(t_cut, x_cut, color="orange", label="numerical")
-    ax1.plot(t_cut, -x_cut, color="orange")
-    ax1.plot(t_cut, analytical_approx(t_cut, 10, 0.9), linestyle="--", color="k", label="analytical")
-    ax1.plot(t_cut, -analytical_approx(t_cut, 10, 0.9), linestyle="--", color="k")
+    ax1.plot(t_cut, 
+             -x_cut, 
+             color="orange", 
+             label="numerical")
     
-    ax2.plot(t_cut, dE_dr,  label="$\epsilon$ = " + str(np.round(E[i], 3))+"$\lambda$")
-    ax4.plot(x_cut, (1/2)*v_cut**2)
+    ax1.plot(t_cut, analytical_approx(t_cut, 0.1, 10, E[i]), 
+             linestyle="dashed", 
+             color="k", 
+             label="analytical")
+    
+    ax1.axhline(phi_0, 
+                linestyle="-.", 
+                color="r", 
+                label= "$\phi_0$")
+    
+    ax2.plot(t_cut, 
+             dE_dr,  
+             label="$\epsilon$ = " + str(np.round(E[i], 3))+"$\lambda$")
+    
+    ax4.plot(x_cut, 
+             potential(x_cut,E[i],r))
     energy = simpson(dE_dr, dx=0.01)
     S_Euclidean.append(energy)
     
-
 def Z(E): 
     return E**(1/4)
 
@@ -163,9 +178,17 @@ def arcsech(x):
 
 
 #Z_2 kink solution:
-ax3.axhline((1-r)**(3/2)*2*np.sqrt(2)/3, label="$Z_2$ kink", linestyle="--", color="k")
-ax3.plot(E, (1-r)**(3/2)*2*np.sqrt(2)/3 -(1/6 + np.log(16*np.sqrt(2)/E)*(1-r)**(3/2))*E/np.sqrt(2), label="analytical approx.", linestyle="-.")
-ax3.plot(E, S_Euclidean, label="numerical")
+ax3.axhline((1-r)**(3/2)*2*np.sqrt(2)/3, 
+            label="$Z_2$ kink", 
+            linestyle="--", 
+            color="k")
+
+ax3.plot(E, (1-r)**(3/2)*2*np.sqrt(2)/3 -(1/6 + np.log(16*np.sqrt(2)/E)*(1-r)**(3/2))*E/np.sqrt(2), 
+         label="analytical approx.", 
+         linestyle="-.")
+
+ax3.plot(E, S_Euclidean, 
+         label="numerical")
 
 """___________________________________Styles_______________________________"""
 
@@ -176,25 +199,42 @@ else:
     density_title = str(r) + "$\mu^2M^2$"
 
 s=11
-ax1.set_ylim(-1.1, 1.1)
-ax1.set_xlim(0, 30)
-ax1.tick_params(axis='both', which='major', labelsize=s)
-ax1.set_title(r"Kinks with several values of $\epsilon$ ( $\rho$ = " + density_title + ' )')
-ax1.set_xlabel('$\sqrt{\lambda}\eta$r', size=20)
-ax1.set_ylabel('$\phi/\eta$', size=20)
+ax1.set_ylim(0.90, 1.025)
+ax1.set_xlim(0, 3)
+ax1.tick_params(axis='both', 
+                which='major', 
+                labelsize=s)
+
+#ax1.set_title(r"Kinks with several values of $\epsilon$ ( $\rho$ = " + density_title + ' )')
+ax1.set_title(r"Background field with $\rho_s = $" + str(10) + '$\mu^2M^2$ and $r_s = $' + str(0.1) +"$\lambda_0$")
+ax1.set_xlabel('$r/\lambda_0$', 
+               size=20)
+
+ax1.set_ylabel('$\phi/\phi_0$', 
+               size=20)
 
 ax2.set_title(r'Energy density for several $\epsilon$ ( $\rho$ = ' + density_title +' )')
-ax2.set_xlabel('$\sqrt{\lambda}\eta$x', size=20)
-ax2.set_ylabel(r'Energy density/$\sqrt{\lambda}\eta^3$', size=20)
+ax2.set_xlabel('$\sqrt{\lambda}\eta$x', 
+               size=20)
+ax2.set_ylabel(r'Energy density/$\sqrt{\lambda}\eta^3$', 
+               size=20)
 ax2.set_ylim(0, 0.51)
 
 ax3.set_title("Energy of the kinks vs. $\epsilon$")
-ax3.set_ylabel("$E_{min}/\sqrt{\lambda}\eta^3$", size=20)
-ax3.set_xlabel("$\epsilon/\lambda $", size = 20)
+ax3.set_ylabel("$E_{min}/\sqrt{\lambda}\eta^3$", 
+               size=20)
+ax3.set_xlabel("$\epsilon/\lambda $", 
+               size = 20)
 
-ax1.legend(loc='center right', fontsize=s)
-ax2.legend(loc='center right', fontsize=s)
-ax3.legend(loc='upper right', fontsize=s)
+ax1.legend(loc='upper right', 
+           fontsize=s)
+ax2.legend(loc='center right', 
+           fontsize=s)
+ax3.legend(loc='upper right', 
+           fontsize=s)
+
+ax4.set_xlabel("$phi_0$")
+ax4.set_ylabel("$\dot{phi}$")
 
 #always have this: 
 plt.show()
